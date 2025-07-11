0d93cb0c70e7f83763b72a8bb66f9c0bbfabf862
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-11 00:49:39 -0400

    Update login.js

--- ShoesMix-Site/Templates/js/login.js
+++ ShoesMix-Site/Templates/js/login.js
@@ -1,1 +1,13 @@
+<script>
+    const senhaInput = document.getElementById('senha');
+    const iconeOlho = document.getElementById('iconeOlhoLogin');
+
+    iconeOlho.addEventListener('click', () => {
+      const tipo = senhaInput.type === 'password' ? 'text' : 'password';
+      senhaInput.type = tipo;
+      iconeOlho.src = tipo === 'text'
+        ? "{{ url_for('static', filename='olho-login-fechado.png') }}"
+        : "{{ url_for('static', filename='olho-login.png') }}";
+    });
+  </script>
 
