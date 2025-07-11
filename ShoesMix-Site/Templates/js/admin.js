c8de19aa372cefb0aeb6d12aba9e3a658569dda3
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-11 00:51:06 -0400

    Update admin.js

--- ShoesMix-Site/Templates/js/admin.js
+++ ShoesMix-Site/Templates/js/admin.js
@@ -1,1 +1,17 @@
+<script>
+    // Ao clicar no botão de configurações, alternar a exibição do painel
+    document.getElementById('config-button').addEventListener('click', function() {
+      const menuConfig = document.getElementById("menu-config");
+      menuConfig.classList.toggle('ativo'); // Alterna a visibilidade do painel
+    });
+
+    // Fechar o painel se clicar fora dele
+    window.addEventListener('click', function(e) {
+      const menuConfig = document.getElementById("menu-config");
+      const configButton = document.getElementById("config-button");
+      if (!menuConfig.contains(e.target) && !configButton.contains(e.target)) {
+        menuConfig.classList.remove('ativo');
+      }
+    });
+  </script>
 
