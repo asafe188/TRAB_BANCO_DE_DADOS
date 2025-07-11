29e1630af97fbf086e1a46e5141efecd77b78ff1
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-11 00:51:54 -0400

    Update cadastro.js

--- ShoesMix-Site/Templates/js/cadastro.js
+++ ShoesMix-Site/Templates/js/cadastro.js
@@ -1,1 +1,18 @@
+<script>
+    function proximaEtapa() {
+      document.getElementById("etapa1").style.display = "none";
+      document.getElementById("etapa2").style.display = "block";
+      document.getElementById("subtitulo").textContent = "Endereço";
+    }
 
+    const senhaInputCadastro = document.getElementById('senha');
+    const iconeOlhoCadastro = document.getElementById('iconeOlhoCadastro');
+
+    iconeOlhoCadastro.addEventListener('click', () => {
+      const tipo = senhaInputCadastro.type === 'password' ? 'text' : 'password';
+      senhaInputCadastro.type = tipo;
+      iconeOlhoCadastro.src = tipo === 'text'
+        ? "{{ url_for('static', filename='olho-cadastro-fechado.png') }}"
+        : "{{ url_for('static', filename='olho-cadastro.png') }}";
+    });
+  </script>
