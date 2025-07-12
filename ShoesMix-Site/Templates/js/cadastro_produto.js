bc52dc984e3b58692e9d14d31859db37413ecf79
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-12 18:26:19 -0400

    Update cadastro_produto.js

--- ShoesMix-Site/Templates/js/cadastro_produto.js
+++ ShoesMix-Site/Templates/js/cadastro_produto.js
@@ -1,1 +1,14 @@
-
+ <script>
+    // Mostrar/ocultar menu flutuante
+    document.getElementById('config-button').addEventListener('click', function() {
+      const menuConfig = document.getElementById("menu-config");
+      menuConfig.classList.toggle('ativo');
+    });
+    window.addEventListener('click', function(e) {
+      const menuConfig = document.getElementById("menu-config");
+      const configButton = document.getElementById("config-button");
+      if (!menuConfig.contains(e.target) && !configButton.contains(e.target)) {
+        menuConfig.classList.remove('ativo');
+      }
+    });
+  </script>
