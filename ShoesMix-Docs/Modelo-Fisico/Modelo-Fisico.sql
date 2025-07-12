35171f357e3531e4d6e381deb7044f1359c21b57
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-11 23:39:02 -0400

    Update and rename Modelo-Fisico.md to Modelo-Fisico.sql

---
+++ ShoesMix-Docs/Modelo-Fisico/Modelo-Fisico.sql
@@ -0,0 +1,27 @@
+-- Tabela CLIENTES
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome_completo VARCHAR(150) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha_hash VARCHAR(255) NOT NULL,
+    cpf VARCHAR(14) NOT NULL UNIQUE,
+    telefone VARCHAR(20),
+    bairro VARCHAR(100),
+    rua VARCHAR(100),
+    numero VARCHAR(10),
+    complemento VARCHAR(100),
+    uf CHAR(2),
+    cidade VARCHAR(100),
+    cep VARCHAR(10)
+);
+
+-- Tabela PRODUTOS
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    preco DECIMAL(10,2) NOT NULL,
+    categoria VARCHAR(50),
+    quantidade_estoque INT DEFAULT 0,
+    imagem_url VARCHAR(255)
+);
