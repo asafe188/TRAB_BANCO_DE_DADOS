3c889a9fad5587d2cda07518d1221dfea10123d4
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-15 20:58:49 -0400

    Create perfil.js

---
+++ ShoesMix-Site/Templates/js/perfil.js
@@ -0,0 +1,14 @@
+  <script>
+    function toggleMenu(event) {
+      event.stopPropagation();
+      const menu = document.getElementById("menuConta");
+      menu.classList.toggle("ativo");
+    }
+    window.addEventListener("click", function (e) {
+      const menu = document.getElementById("menuConta");
+      const toggle = document.querySelector(".dropdown-toggle");
+      if (!toggle.contains(e.target)) {
+        menu.classList.remove("ativo");
+      }
+    });
+  </script>
