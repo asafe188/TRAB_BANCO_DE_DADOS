b9075c8b753d48fcae6e8c8fbc51e883f194b241
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-15 20:54:41 -0400

    Create estoque.js

---
+++ ShoesMix-Site/Templates/js/estoque.js
@@ -0,0 +1,14 @@
+  <script>
+    document.getElementById('config-button').addEventListener('click', function(event) {
+      event.stopPropagation();
+      const menuConfig = document.getElementById("menu-config");
+      menuConfig.classList.toggle('ativo');
+    });
+    window.addEventListener("click", function(e) {
+      const menuConfig = document.getElementById("menu-config");
+      const configButton = document.getElementById("config-button");
+      if (!menuConfig.contains(e.target) && !configButton.contains(e.target)) {
+        menuConfig.classList.remove("ativo");
+      }
+    });
+  </script>
