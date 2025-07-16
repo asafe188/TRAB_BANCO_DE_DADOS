78857e427b13cfa46d5a44686e07277de891d394
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-15 20:56:01 -0400

    Create meus_pedidos.js

---
+++ ShoesMix-Site/Templates/js/meus_pedidos.js
@@ -0,0 +1,17 @@
+  <script>
+    function mudarAba(aba) {
+      window.location.href = "/meus_pedidos?aba=" + aba;
+    }
+    function toggleMenu(event) {
+      event.stopPropagation();
+      const menu = document.getElementById("menuConta");
+      menu.classList.toggle("ativo");
+    }
+    window.addEventListener("click", function(e) {
+      const menu = document.getElementById("menuConta");
+      const toggle = document.querySelector(".dropdown-toggle");
+      if (!toggle.contains(e.target)) {
+        menu.classList.remove("ativo");
+      }
+    });
+  </script>
