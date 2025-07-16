c49de457986dab0a122d15cc87a0110f02f5c0b0
vitor Oliveira <joaovitor2244jv@gmail.com>
2025-07-15 20:41:55 -0400

    Update index.js

--- ShoesMix-Site/Templates/js/index.js
+++ ShoesMix-Site/Templates/js/index.js
@@ -1,10 +1,10 @@
-<script>
+  <script>
     function toggleMenu(event) {
       event.stopPropagation();
       const menu = document.getElementById("menuConta");
       menu.classList.toggle("ativo");
     }
-    window.addEventListener("click", function (e) {
+    window.addEventListener("click", function(e) {
       const menu = document.getElementById("menuConta");
       const toggle = document.querySelector(".dropdown-toggle");
       if (!toggle.contains(e.target)) {
